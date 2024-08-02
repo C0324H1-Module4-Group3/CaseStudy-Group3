c61ee77bfebb5bc0b7b9edf1a822ec1026c3a1f3
levancuon <levancuongprovip@gmail.com>
2024-08-02 22:18:42 +0700

    fix

---
+++ src/main/java/com/example/casestudymodule4/controller/Cart.java
@@ -0,0 +1,27 @@
+package com.example.casestudymodule4.controller;
+
+import com.example.casestudymodule4.model.User;
+import com.example.casestudymodule4.service.IUserService;
+import com.example.casestudymodule4.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/cart")
+public class Cart {
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("")
+    private String showCart() {
+
+        return "/cart";
+    }
+}
+//User user = userService.findById(id);
+//        model.addAttribute("user",user);
---
+++ src/main/java/com/example/casestudymodule4/service/impl/UserService.java
@@ -0,0 +1,19 @@
+package com.example.casestudymodule4.service.impl;
+
+import com.example.casestudymodule4.model.User;
+import com.example.casestudymodule4.repository.UserRepo;
+import com.example.casestudymodule4.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public User findById(Long id) {
+        return userRepo.findById(id).orElse(null);
+    }
+}
