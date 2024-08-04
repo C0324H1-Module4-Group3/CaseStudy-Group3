6143c6024e134cd8fe88093e9eacf1e621b6d10c
Truong177 <truongtranquang773@gmail.com>
2024-08-04 11:53:47 +0700

    giao dien admin

--- src/main/java/com/example/casestudymodule4/service/impl/ProductService.java
+++ src/main/java/com/example/casestudymodule4/service/impl/ProductService.java
@@ -1,40 +1,52 @@
 package com.example.casestudymodule4.service.impl;
 
-import com.example.casestudymodule4.model.Product;
+import com.example.casestudymodule4.model.SkuProduct;
+import com.example.casestudymodule4.repository.IProductRepository;
 import com.example.casestudymodule4.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductService implements IProductService {
+    @Autowired
+    private IProductRepository productRepository;
+
     @Override
-    public List<Product> findAll() {
+    public List<SkuProduct> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public void save(SkuProduct product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public Page<SkuProduct> findAll(String name, Pageable pageable) {
         return null;
     }
 
     @Override
-    public void save(Product product) {
-
+    public Page<SkuProduct> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
     @Override
-    public Page<Product> findAll(String name, Pageable pageable) {
-        return null;
+    public SkuProduct findById(Long id) {
+        return productRepository.findById(id).orElse(null);
     }
 
     @Override
-    public Product findById(Long id) {
-        return null;
+    public void delete(SkuProduct product) {
+        productRepository.delete(product);
     }
 
     @Override
-    public void delete(Product product) {
-
-    }
-
-    @Override
-    public void update(Product product) {
-
+    public void update(SkuProduct product) {
+        productRepository.save(product);
     }
 }
