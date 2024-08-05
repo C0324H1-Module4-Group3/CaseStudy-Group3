266a5c528c96a50736211e7e5556414fb6529507
thanhlong279 <thanhlong2791992@gmail.com>
2024-08-05 08:23:37 +0700

    manager-order

---
+++ src/main/java/com/example/casestudymodule4/dto/OrderDetailDTO.java
@@ -0,0 +1,14 @@
+package com.example.casestudymodule4.dto;
+
+public interface OrderDetailDTO {
+    Long getIdOrder();
+    String getCustomerName();
+    String getProductName();
+    String getCategoryName();
+    Double getTotalMoney();
+}
+
+
+
+
+
